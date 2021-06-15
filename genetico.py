0ef6ae83764f848f8e78977cb56c2a9a4a77f6c2
Luis <vrl4@outlook.es>
2021-06-14 22:41:17 -0600

    fix: Fixing out of range

--- genetico.py
+++ genetico.py
@@ -48,7 +48,7 @@ def create_individual():
     decimal_dna = []
     branches = sorted([random.randint(0,3), random.randint(0,3)])
     fork_angle = sorted([random.randint(10,90), random.randint(10, 90)])
-    base_length = sorted([random.randint(20, 150), random.randint(20,150)])
+    base_length = sorted([random.randint(20, 300), random.randint(20,300)])
     individual = [random.randint(2, 8), format(random.randint(20, 100), '3d'), 
                     format(random.randint(10,99), '2d'), format(random.randint(10,99), '2d'),
                     format(random.randint(10,99), '2d'), branches[0], branches[1],
@@ -91,9 +91,27 @@ def translate_individual(individual):
     branches = [int(individual[10]), int(individual[11])]
     fork_angle = [int(list_to_int(individual[12:15])), int(list_to_int(individual[15:18]))]
     base_length = [int(list_to_int(individual[18:21])), int(list_to_int(individual[21:25]))]
+    print(branches, decrement, depth, diameter, fork_angle, base_length, base_decrement, branch_decrement, "b")
+    if diameter > 100:
+        diameter = 100
+    if branches[0] > 3:
+        branches[0] = 3
+    if branches[1] > 3:
+        branches[1] = 3
+    if base_length[0] > 300:
+        base_length[0] = 300
+    if base_length[1] > 300:
+        base_length[1] = 300
+    if base_decrement >= 1:
+        base_decrement = 0.99
+    if decrement >= 1:
+        decrement = 0.9
+    if branch_decrement >= 1:
+        print("entro")
+        branch_decrement = 0.9
+    if depth > 9:
+        depth = 9
     print(branches, decrement, depth, diameter, fork_angle, base_length, base_decrement, branch_decrement)
-    if base_decrement > 0:
-        base_decrement = 0.99999
     draw_tree(300, 550, 270, branches, decrement, depth, diameter, fork_angle, base_length, base_decrement, branch_decrement)
 
 def mutate(probability):
@@ -134,7 +152,7 @@ def input(event):
         exit(0)
 
 def main():
-    generations = 2 
+    generations = 10 
     initial_population = 100 
     generate_population(initial_population)
     
